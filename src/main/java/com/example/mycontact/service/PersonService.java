25a5867f2e0e8fcfd51174a77b0ffba36b63ae2c
Kimyechan <vlvkcjswo7@naver.com>
2020-09-25 22:31:51 +0900

    Feat: JPA Relation OneToOne
    cascade, fetch concept

--- src/main/java/com/example/mycontact/service/PersonService.java
+++ src/main/java/com/example/mycontact/service/PersonService.java
@@ -4,13 +4,16 @@ import com.example.mycontact.domain.Block;
 import com.example.mycontact.domain.Person;
 import com.example.mycontact.repository.BlockRepository;
 import com.example.mycontact.repository.PersonRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class PersonService {
     @Autowired
     private PersonRepository personRepository;
@@ -24,6 +27,16 @@ public class PersonService {
 //        List<String> blockNames = blocks.stream().map(Block::getName).collect(Collectors.toList());
 //
 //        return people.stream().filter(person -> blockNames.contains(person.getName())).collect(Collectors.toList());
-        return people.stream().filter(person -> person.getBlock() == null).collect(Collectors.toList());
+        return people.stream().filter(person -> person.getBlock() != null).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public Person getPerson(Long id){
+        Person person = personRepository.findById(id).get();
+
+        System.out.println("person : " + person);
+        log.info("person : {}", person);
+
+        return person;
     }
 }
