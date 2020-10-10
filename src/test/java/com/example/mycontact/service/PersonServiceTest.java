c904b05892ed5f0ce7075a360d2d53128be4d671
Kimyechan <vlvkcjswo7@naver.com>
2020-10-10 13:02:39 +0900

    Feat: Person service test getPerson put
    how to use verify

--- src/test/java/com/example/mycontact/service/PersonServiceTest.java
+++ src/test/java/com/example/mycontact/service/PersonServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.mycontact.service;
 
+import com.example.mycontact.controller.dto.PersonDto;
 import com.example.mycontact.domain.Person;
 import com.example.mycontact.repository.PersonRepository;
 import org.assertj.core.util.Lists;
@@ -11,10 +12,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
@@ -32,5 +36,34 @@ class PersonServiceTest {
 
         assertThat(result.size()).isEqualTo(1);
         assertThat(result.get(0).getName()).isEqualTo("martin");
+    }
+
+    @Test
+    void getPerson() {
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("martin")));
+
+        Person person = personService.getPerson(1L);
+
+        assertThat(person.getName()).isEqualTo("martin");
+    }
+
+    @Test
+    void getPersonIfNotFound() {
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.empty());
+
+        Person person = personService.getPerson(1L);
+
+        assertThat(person).isNull();
+    }
+
+    @Test
+    void put() {
+        PersonDto dto = PersonDto.of("martin", "programming", "판교", LocalDate.now(), "programmer", "010-1111-2222");
+
+        personService.put(dto);
+
+        verify(personRepository, times(1)).save(any(Person.class));
     }
 }
